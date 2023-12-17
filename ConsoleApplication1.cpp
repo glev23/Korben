94fafdb8abaa5457c31d3bc2286cef4550d5e647
glev23 <101514295+glev23@users.noreply.github.com>
2023-12-17 15:30:13 +0300

    Add files via upload

--- ConsoleApplication1.cpp
+++ ConsoleApplication1.cpp
@@ -12,11 +12,11 @@
 
 using namespace std;
 
-std::string read_ini_file(const std::string& filename, const std::string& key) {
-    std::ifstream file(filename);
-    std::string line;
+string read_ini_file(const string& filename, const string& key) {
+    ifstream file(filename);
+    string line;
 
-    while (std::getline(file, line)) {
+    while (getline(file, line)) {
         if (line.find(key) != string::npos) {
             size_t start = line.find('=') + 1;
             size_t end = line.length();
@@ -28,25 +28,25 @@ std::string read_ini_file(const std::string& filename, const std::string& key) {
 }
 
 struct Client {
-    std::string fullName;
-    std::string phoneNumber;
-    std::string startDate;
-    std::string endDate;
+    string fullName;
+    string phoneNumber;
+    string startDate;
+    string endDate;
     int debt;
     int creditLimit;
 };
 
 struct Service {
-    std::string name;
+    string name;
     int code;
     double rate;
-    std::string interval;
+    string interval;
 };
 
 struct Usage {
-    std::string phoneNumber;
+    string phoneNumber;
     int serviceCode;
-    std::string dateTime;
+    string dateTime;
     int durationSeconds;
 };
 
@@ -54,8 +54,8 @@ int SecondsInMonth() {
     //Cчитаем сколько секунд в месяце
     int month;
     int yearday;
-    std::time_t now = std::time(NULL);
-    std::tm* localTime = std::localtime(&now);
+    time_t now = time(NULL);
+    tm* localTime = localtime(&now);
     month = localTime->tm_mon + 1;
     yearday = localTime->tm_yday + 1;
     if (month == 1 || month == 3 || month == 5 || month == 7 || month == 8 || month == 10 || month == 12) {
@@ -74,13 +74,13 @@ int SecondsInMonth() {
     }
 }
 
-bool IsDateInCurrentMonth(const std::string& dateTime) {
+bool IsDateInCurrentMonth(const string& dateTime) {
     // Получаем текущую дату и время
-    std::time_t now = std::time(NULL);
-    std::tm* localTime = std::localtime(&now);
+    time_t now = time(NULL);
+    tm* localTime = localtime(&now);
 
     // Парсим dateTime
-    std::istringstream ss(dateTime);
+    istringstream ss(dateTime);
     int year, month, day, hour, minute, second;
     char delimiter;
     ss >> year >> delimiter >> month >> delimiter >> day >> hour >> delimiter >> minute >> delimiter >> second;
@@ -90,29 +90,29 @@ bool IsDateInCurrentMonth(const std::string& dateTime) {
 }
 
 int main() {
-    std::vector<Client> clients;
-    std::vector<Service> services;
-    std::vector<Usage> usages;
-    std::vector<int> range;
-    std::map<int, double> serviceRates;
+    vector<Client> clients;
+    vector<Service> services;
+    vector<Usage> usages;
+    vector<int> range;
+    map<int, double> serviceRates;
     //setlocale(LC_ALL, "Russian");
 
     // Чтение информации о клиентах
-    std::ifstream clientsFile("Clients.txt");
+    ifstream clientsFile("Clients.txt");
     if (clientsFile.is_open()) {
-        std::string line;
-        while (std::getline(clientsFile, line)) {
-            std::istringstream ss(line);
-            std::string token;
+        string line;
+        while (getline(clientsFile, line)) {
+            istringstream ss(line);
+            string token;
             Client client;
-            for (int i = 0; std::getline(ss, token, ','); ++i) {
+            for (int i = 0; getline(ss, token, ','); ++i) {
                 switch (i) {
                 case 0: client.fullName = token; break;
                 case 1: client.phoneNumber = stod(token); break;
                 case 2: client.startDate = token; break;
                 case 3: client.endDate = token; break;
-                case 4: client.debt = std::stoi(token); break;
-                case 5: client.creditLimit = std::stoi(token); break;
+                case 4: client.debt = stoi(token); break;
+                case 5: client.creditLimit = stoi(token); break;
                 }
             }
             clients.push_back(client);
@@ -120,25 +120,25 @@ int main() {
         clientsFile.close();
     }
     else {
-        std::cerr << "Error: Unable to open clients file." << std::endl;
+        cout << "Error: Unable to open clients file." << endl;
         return 1;
     }
 
 
     // Чтение информации об услугах
-    std::ifstream servicesFile("Services.txt");
+    ifstream servicesFile("Services.txt");
 
     if (servicesFile.is_open()) {
-        std::string line;
-        while (std::getline(servicesFile, line)) {
-            std::istringstream ss(line);
-            std::string token;
+        string line;
+        while (getline(servicesFile, line)) {
+            istringstream ss(line);
+            string token;
             Service service;
-            for (int i = 0; std::getline(ss, token, ','); ++i) {
+            for (int i = 0; getline(ss, token, ','); ++i) {
                 switch (i) {
                 case 0: service.name = token; break;
-                case 1: service.code = std::stoi(token); break;
-                case 2: service.rate = std::stod(token); break;  // Используем stod для считывания значения с плавающей точкой
+                case 1: service.code = stoi(token); break;
+                case 2: service.rate = stod(token); break;  // Используем stod для считывания значения с плавающей точкой
                 case 3: service.interval = token; break;
                 }
             }
@@ -147,25 +147,25 @@ int main() {
         servicesFile.close();
     }
     else {
-        std::cerr << "Error: Unable to open services file." << std::endl;
+        cout << "Error: Unable to open services file." << endl;
         return 1;
     }
 
 
     // Чтение информации об использовании услуг
-    std::ifstream usageFile("Usage.txt");
+    ifstream usageFile("Usage.txt");
     if (usageFile.is_open()) {
-        std::string line;
-        while (std::getline(usageFile, line)) {
-            std::istringstream ss(line);
-            std::string token;
+        string line;
+        while (getline(usageFile, line)) {
+            istringstream ss(line);
+            string token;
             Usage usage;
-            for (int i = 0; std::getline(ss, token, ','); ++i) {
+            for (int i = 0; getline(ss, token, ','); ++i) {
                 switch (i) {
                 case 0: usage.phoneNumber = stod(token); break;
-                case 1: usage.serviceCode = std::stoi(token); break;
+                case 1: usage.serviceCode = stoi(token); break;
                 case 2: usage.dateTime = token; break;
-                case 3: usage.durationSeconds = std::stoi(token); break;
+                case 3: usage.durationSeconds = stoi(token); break;
                 }
             }
             if (IsDateInCurrentMonth(usage.dateTime)) { // Проверяем, что дата в текущем месяце
@@ -175,16 +175,16 @@ int main() {
         usageFile.close();
     }
     else {
-        std::cerr << "Error: Unable to open usage file." << std::endl;
+        cout << "Error: Unable to open usage file." << endl;
         return 1;
     }
 
     // Чтение диапазона параметров.
-    std::string filename = "Param.ini";
-    std::string key1 = "key1";
-    std::string key2 = "key2";
-    std::string value1 = read_ini_file(filename, key1);
-    std::string value2 = read_ini_file(filename, key2);
+    string filename = "Param.ini";
+    string key1 = "key1";
+    string key2 = "key2";
+    string value1 = read_ini_file(filename, key1);
+    string value2 = read_ini_file(filename, key2);
 
     // Создание словаря тарифов услуг
     for (const auto& service : services) {
@@ -212,9 +212,9 @@ int main() {
     }
 
     // Вычисление потраченной суммы.
-    std::ofstream reportFile("Report.txt");
+    ofstream reportFile("Report.txt");
     bool flag = false;
-    std::map<int, double> totalCostPerService;
+    map<int, double> totalCostPerService;
     for (const auto& usage : usages) {
         for (const auto& client : clients) {
             if (client.phoneNumber == usage.phoneNumber) {
@@ -232,7 +232,7 @@ int main() {
 
     for (const auto& pair : totalCostPerService) {
         // Ищем название сервиса по его коду
-        std::string serviceName;
+        string serviceName;
         for (const auto& service : services) {
             if (service.code == pair.first) {
                 serviceName = service.name;
@@ -240,7 +240,7 @@ int main() {
             }
         }
         flag = true;
-        reportFile << "Сервис: " << serviceName << " (" << pair.first << "), Сумма: " << pair.second << " рублей" << std::endl;
+        reportFile << "Сервис: " << serviceName << " (" << pair.first << "), Сумма: " << pair.second << " рублей" << endl;
     }
 
     if (flag) {
